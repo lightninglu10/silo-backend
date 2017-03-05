3aadb38e3ae4fa797c63f2c57ab63b278400a0c5
Patrick Lu <lightning.lu7@gmail.com>
2017-03-04 20:15:10 -0800

    removed USERNAME_FIELD=None

--- silo/settings.py
+++ silo/settings.py
@@ -141,11 +141,10 @@ REST_FRAMEWORK = {
 WSGI_APPLICATION = 'silo.wsgi.application'
 
 # Django AllAuth Setup
-ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_SIGNUP_FORM_CLASS = 'contacts.forms.SignupForm'
+# ACCOUNT_SIGNUP_FORM_CLASS = 'contacts.forms.SignupForm'
 ACCOUNT_EMAIL_VERIFICATION = False
 
 # TODO: get a proper email backend so we can send authentication emails
