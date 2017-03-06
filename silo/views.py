1cdd89f9615f3d5671328d7d909d851ddfd94f59
Patrick Lu <lightning.lu7@gmail.com>
2017-03-06 15:22:30 -0800

    removed authentication from health check

--- silo/views.py
+++ silo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 
 def index(request):
     """
@@ -10,6 +10,7 @@ def index(request):
     return render(request, 'index.tpl.html')
 
 @api_view(['GET'])
+@permission_classes(())
 def healthcheck(request):
     """
     Health check for elastic beanstalk
